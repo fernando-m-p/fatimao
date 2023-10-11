6898b5bb91b707f56ddd1dd7d42ec2afd63f44ab
FERNANDO MORAIS PEIXOTO <fernando.peixoto@trt22.jus.br>
2023-10-11 17:09:29 -0300

    feat: interfaces for core

---
+++ app/model/interfaces.tsx
@@ -0,0 +1,38 @@
+export type Time = {
+    nome:string;
+    escudo: string;
+    abreviado:string;
+}
+
+export type Jogador = {
+    nome:string;
+    time:Time;
+}
+
+export type JogoTime = {
+    time:Time;
+    gols:Jogador[];
+    amarelos:Jogador[];
+    azuis:Jogador[];
+    vermelhos:Jogador[];
+}
+
+export type Jogo = {
+    mandante:JogoTime;
+    visitante:JogoTime;
+    finalizado:boolean;
+    local:string;
+    data:string;
+    hora:string;
+}
+
+export type Rodada = {
+    jogos: Jogo[];
+    descricao:string;
+}
+
+export type Turno = {
+    rodadas: Rodada[]
+}
+
+
